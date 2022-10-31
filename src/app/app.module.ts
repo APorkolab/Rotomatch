2511183c27caa5f6c7c4f7f2a80e197d4f9b4fa0
APorkolab <73812292+APorkolab@users.noreply.github.com>
2022-10-31 07:43:28 +0100

    Generate Angular components and service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RulesComponent } from './page/rules/rules.component';
+import { GameComponent } from './page/game/game.component';
+import { ContactComponent } from './page/contact/contact.component';
+import { HomeComponent } from './page/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RulesComponent,
+    GameComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
